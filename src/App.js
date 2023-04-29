8edd29ce19775a07c566ebd6991142b4638b64c6
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-04-29 15:33:48 +0500

    create Flex component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import Title from "./components/Title";
+import Flex from "./components/Flex";
 
 const AppWrapper = styled.div`
-width: 100%;
-min-height: 100vh;
-padding: 2rem;
-background: black;
-color: white;
-`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background: black;
+  color: white;
+`;
 
 const App = () => {
   return (
-  <AppWrapper>
-    <Title color="green">Test 123456</Title>
+    <AppWrapper>
+      <Flex justify='center'>
+        <Title color="green">Test 123456</Title>
+      </Flex>
     </AppWrapper>
   );
 };
