b27278654ce46d33282164506be3c27c2b699e6b
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-04-29 15:40:56 +0500

    create Console component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Title from "./components/Title";
 import Flex from "./components/Flex";
+import Console from "./components/Console";
 
 const AppWrapper = styled.div`
   width: 100%;
@@ -17,6 +18,7 @@ const App = () => {
       <Flex justify='center'>
         <Title color="green">Test 123456</Title>
       </Flex>
+      <Console color='green'/>
     </AppWrapper>
   );
 };
